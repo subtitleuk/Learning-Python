aff54c38cf5fc6aa346ef6023518198b45289c2b
Brendon Higgins <b_d_h@hotmail.com>
2021-02-01 18:02:19 +0000

    2nd part

--- Azure Deep Dive/tables_demo.py
+++ Azure Deep Dive/tables_demo.py
@@ -120,34 +120,3 @@ print("\nAdding entity to table, '" + new_table_name + "'...")
 table_service.insert_entity(new_table_name, course_details)
 
 print("\n===== DONE =====")
-Demonstration code: tables_batch.py
-
-import time
-
-table_name = "coursecatalog"
-connect_str = "DefaultEndpointsProtocol=https;AccountName=latajlstore1;AccountKey=kU+w12g3WdyPiN8d/QiQvpeFYgIt0yxuSY8uJ6p1cMZR/uARJLrackSiuTqOiVCeOLjWCt0OLyZf+UtISzbeqw==;EndpointSuffix=core.windows.net"
-
-from azure.cosmosdb.table.tableservice import TableService
-from azure.cosmosdb.table.models import Entity 
-
-# Let's import the batch library
-from azure.cosmosdb.table.tablebatch import TableBatch
-
-# Connect to the Table service
-table_service = TableService(connection_string=connect_str)
-print("\nConnected to Table service")
-
-# Let's perform a batch transaction, and add two course expiry dates
-batch = TableBatch()
-task001 = {'PartitionKey': 'Azure', 'RowKey': '280',
-    'expiryDate': '1 July 2020'}
-task002 = {'PartitionKey': 'Azure', 'RowKey': '381',
-    'expiryDate': '1 July 2020'}
-
-# Perform batch transaction using MERGE (could be update, insert, etc)
-batch.merge_entity(task001)
-batch.merge_entity(task002)
-table_service.commit_batch(table_name, batch)
-
-print("\nBatch transaction complete")
-print("=======================")
